6cba930d89e407e0aa2d548c656470d420aee771
Kevin Zhugang <zhugang.kevin@gmail.com>
2025-12-03 19:40:46 +0800

    Updated App.jsx with EnvCheck component

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          I am testing {count}
+          I am once {count}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
